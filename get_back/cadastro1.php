89167095d4b60e5e36a19e45d5cceb95b16350bf
jeffnogueira <30880938+jeffnogueira@users.noreply.github.com>
2017-09-21 21:07:01 -0300

    Add files via upload

---
+++ get_back/cadastro1.php
@@ -0,0 +1,64 @@
+<?php session_start();
+    //verifica se existe dados na sessao
+    if(isset($_SESSION['signErro'])) {
+      $sign_erro = $_SESSION['signErro'];
+    } else {
+      header("Location: cadastro.php");
+    }
+    include("conexao.php");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include("head.php"); ?>
+    </head>
+
+    <body style="background-image: url(img/wall2.jpg);background-repeat: no-repeat;">
+        <section>
+            <div class="container">
+                <div class="row">
+                    <br><br><br><br><br><br>
+                    <div class="col-md-4 col-sm-12">
+                        <?php echo $sign_erro; ?>
+                        <form method="POST" action="validarcadastro.php" enctype="multipart/form-data">
+                            <div class="form-group">
+                                <label>E-mail</label>
+                                <input name="email" type="email" class="form-control" placeholder="E-mail" required>
+                                <small id="emailHelp" class="form-text text-muted"></small>
+                            </div>
+                            <div class="form-group">
+                                <label>Senha</label>
+                                <input name="senha" type="password" class="form-control" placeholder="Senha" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Nome</label>
+                                <input name="nome" type="text" class="form-control" placeholder="Nome" required>
+                            </div>
+                            <div class="form-group">
+                                <label>Curso</label>
+                                <input name="curso" type="text" class="form-control" placeholder="Curso">                      
+                            </div>
+                            <div class="form-group">
+                                <label>Foto de Perfil</label>
+                                <input type="file" name="img" required/>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary" style="background-color: #dc0909;border-color: #dc0909;">Cadastrar</button>
+                            <a href="login.php"><input type="button" class="btn btn-primary" style="background-color: #dc0909;border-color: #dc0909;float: right;" value="Já possuo uma conta"></a>
+                        </form>
+                    </div>
+                    <div class="col-md-6 col-md-offset-2 col-sm-12" style="font-family: 'Anton', sans-serif;">
+                        <h2>CADASTRE, PROCURE E ACHE SEU ITEM.</h2>
+                        <br>
+                        <h4>A melhor forma de reaver seus itens perdidos.</h4>
+                        <h4 style="float: right;">Com confidencialidade e segurança.</h4>
+                        <br><br>
+                        <h5 style="float: right;color: black;font-family: 'Bitter', serif;"><i>The Things Get Back.</i></h5>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <?php include("script.php"); ?>
+    </body>
+</html>
---
+++ get_back/login1.php
@@ -0,0 +1,51 @@
+<?php session_start();
+    //verifica se existe dados na sessao
+    if(isset($_SESSION['loginErro'])) {
+      $login_erro = $_SESSION['loginErro'];
+    } else {
+      header("Location: login.php");
+    }
+    include("conexao.php");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include("head.php"); ?>
+    </head>
+    <body style="background-image: url(img/wall2.jpg);background-repeat: no-repeat;">
+        <section style="">
+            <div class="container">
+                <div class="row">
+                    <br><br><br><br><br><br><br>
+                    <div class="col-md-6 col-sm-12" style="font-family: 'Anton', sans-serif;">
+                        <h2>CADASTRE, PROCURE E ACHE SEU ITEM.</h2>
+                        <br>
+                        <h4>A melhor forma de reaver seus itens perdidos.</h4>
+                        <h4 style="float: right;">Com confidencialidade e segurança.</h4>
+                        <br><br>
+                        <h5 style="float: right;color: white;font-family: 'Bitter', serif;"><i>The Things Get Back.</i></h5>
+                    </div>
+                    <div class="col-md-4 col-md-offset-2 col-sm-12">
+                        <br><br><br><br>
+                        <form method="post" action="validarlogin.php">
+                            <?php echo $login_erro; ?>
+                            <div class="form-group">
+                                <input name="email" type="email" class="form-control" placeholder="Email" required>
+                                <small id="emailHelp" class="form-text text-muted"></small>
+                            </div>
+                            <div class="form-group">
+                                <input name="senha" type="password" class="form-control" placeholder="Senha" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary" style="background-color: #dc0909;border-color: #dc0909;">Entrar</button>
+                            <a href="cadastro.php" style="float: right;"><input type="button" class="btn btn-primary" style="background-color: #dc0909;border-color: #dc0909;" value="Cadastrar"></a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <?php include("footer.php"); ?>
+
+        <?php include("script.php"); ?>
+    </body>
+</html>
