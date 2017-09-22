1f06ed2eb4c0ba2629cf78902251bb0f47efca50
jeffnogueira <30880938+jeffnogueira@users.noreply.github.com>
2017-09-21 21:15:10 -0300

    Update header.php

--- get_back/header.php
+++ get_back/header.php
@@ -30,7 +30,7 @@
 		        	<li class="dropdown">
 		          		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><img alt="logo" src="img/perfil.jpg" style="width: 40px;height: 35px;border-radius: 20px;"><span class="caret"></span></a>
 		          		<ul class="dropdown-menu">
-			            	<li><a href="#">Perfil</a></li>
+			            	<li><a href="perfil.php">Perfil</a></li>
 				            <li role="separator" class="divider"></li>
 				            <li><a href="logout.php">Sair</a></li>
 		          		</ul>
