b53bd0aeb3270c7582e63c8f4c378168f4034402
guilhermechilon <guilhermechilon@gmail.com>
2017-11-21 23:22:33 -0200

    Add files via upload

---
+++ envia_coment.php
@@ -0,0 +1,18 @@
+<?php
+
+	include_once("seguranca.php");
+
+	
+
+	$text = $_POST['text'];
+	$id_usuario = $_SESSION['id_usuario'];
+	$id_coment = $id_usuario;
+
+	date_default_timezone_set('America/Sao_Paulo');
+	$data = date('Y-m-d H:i:s');	
+
+	$sql = "INSERT INTO comentario(id_coment, id_usuario, comentario, data_coment) VALUES ('$id_coment', '$id_usuario', '$text','$,'$data')";
+	$result = mysqli_query($conn,$sql);
+
+	header("Location:index.php");
+?>
