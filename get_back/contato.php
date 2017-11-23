18681c3283544117bbe10066af39c9e6c1070362
jeffnogueira <30880938+jeffnogueira@users.noreply.github.com>
2017-11-23 16:52:16 -0200

    mudando script para submit.php

--- get_back/contato.php
+++ get_back/contato.php
@@ -1,28 +1,64 @@
-<?php
-// Passando os dados obtidos pelo formulário para as variáveis abaixo
+<?php include_once("seguranca.php"); ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include("head.php"); ?>
+	</head>
+	<body>
+		<?php include("header.php"); ?>
+		<br><br><br><br>
+		<div class="container">
+			<div class="row">
+	            <div class="col-md-12 col-sm-12">
+	             <br><br>
+	                  <div class="col-md-8 col-sm-12" style="text-align:center;">
+	                    <form action="submit.php" method="POST" enctype="multipart/form-data" class="form-horizontal">
+	                      <div class="row">
+	                        <div class="col-md-12 col-sm-12">   
+	                          <br>       
+	                          <div class="form-group">
+	                            <div class="col-md-2 col-sm-2">
+	                              <label for="input-rounded" class="col-sm-2 control-label">Nome</label>
+	                            </div>
+	                            <div class="col-md-6 col-sm-10">
+	                              <input style="border-color: #dc0909;" type="text" class="form-control input-rounded" name="nome">
+	                            </div>
+	                          </div>
+                          <div class="form-group">
+	                            <div class="col-md-6 col-sm-2">
+	                              <label for="input-rounded" class="col-sm-2 control-label">Email</label>
+	                            </div>
+	                            <div class="col-md-3 col-sm-10">
+	                              <input style="border-color: #dc0909;" type="text" class="form-control input-rounded" name="email">
+	                            </div>
+	                          </div>
+                          <div class="form-group">
+	                            <div class="col-md-2 col-sm-2">
+	                              <label for="input-rounded" class="col-sm-2 control-label">Assunto</label>
+	                            </div>
+	                            <div class="col-md-6 col-sm-10">
+	                              <input style="border-color: #dc0909;" type="text" class="form-control input-rounded" name="assunto" >
+	                            </div>
+	                          </div>
+                          <textarea class="form-control" name="text" rows="4" style="resize: none;border-color: #dc0909;" placeholder="Digite sua mensagem" required></textarea>
+	                          <div class="form-group" style="text-align: center;">
+	                            <button type="submit" class="btn btn-default" style="border: solid 1px;border-radius: 15px;border-color: #dc0909;background-color: #dc0909;color: white;">Salvar Mudanças</button> 
+	                          </div>
+	                        </div>
+	                      </div>
+	                    </form>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
 
-$emailremetente    = trim($_POST['email']);
-$emaildestinatario = 'contato@getback.com'; 
-$nome = $_POST['nome'];
-$msg = $_POST['msg'];
-$m['assunto'] = $assunto; // assunto, por algum motivo ele precisa vir dessa forma.
- 
-/* Montando a mensagem a ser enviada no corpo do e-mail. */
-$mensagemHTML = '<P>Contato Cliente</P>
-<p><b>E-Mail:</b> '.$emailremetente.'</p>
-<p><b>Nome:</b> '.$nome.'</p>
-<p><b>Mensagem:</b> '.$msg.'</p>
-<hr>';
+		<?php include("footer.php"); ?>
 
+		<?php include("script.php"); ?>
 
-// O remetente deve ser um e-mail do seu domínio conforme determina a RFC 822.
-// O return-path deve ser ser o mesmo e-mail do remetente.
-$headers = "MIME-Version: 1.1\r\n";
-$headers .= "Content-type: text/html; charset=utf-8\r\n";
-$headers .= "From: $emailremetente\r\n"; // remetente
-$headers .= "$assunto\r\n";
-$envio = mail($emaildestinatario,$m['assunto'],$mensagemHTML, $headers); 
-
-if($envio)
-echo "<script>location.href='contato.php'</script>"; // Página que será redirecionada
-?>
+		<?php include("footer.php"); ?>
+		
+		<style type="text/css">.footer{color: black !important;margin-top: 8px;padding: 5px;position:absolute !important;bottom:0 !important;}</style>
+		
+	</body>
+</html>
